266c8250531ba8555ac2c96556f0d6997c960ed1
Francisco Carlos Molina Duarte Junior <francisco.carlos@dinfor.uniceub>
2025-09-25 10:26:59 -0300

    aulas até o dia 25/09/2025

---
+++ aula2/src/aula2/Principal.java
@@ -0,0 +1,17 @@
+package aula2;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Carro c1 = new Carro();
+		c1.marca = "Pulse";
+		c1.modelo = "Fiat";
+		c1.ano = 2024;
+		c1.preco = 12000;
+		c1.autonomia = 15;
+		
+		System.out.println(c1.modelo);
+	}
+
+}
---
+++ aula3/src/aula3/Pessoa.java
@@ -0,0 +1,10 @@
+package aula3;
+
+public class Pessoa {
+	
+	public String nome;
+	public String dataNascimento;
+	public String endereco;
+	public String telefone;
+
+}
